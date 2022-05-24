1453919914ee431230d53b340d4c6e8a3ede44fd
“aquador” <Allison.quador@gmail.com>
2022-05-24 15:31:51 -0600

    initial page

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -6,20 +6,6 @@ const linkStyle = {
     color: 'white',
     textDecoration: 'none'
 }
-const authenticatedOptions = (
-	<>
-		<Nav.Item>
-			<Link to='change-password' style={linkStyle}>
-				Change Password
-			</Link>
-		</Nav.Item>
-		<Nav.Item>
-			<Link to='sign-out' style={linkStyle}>
-				Sign Out
-			</Link>
-		</Nav.Item>
-	</>
-)
 
 const unauthenticatedOptions = (
 	<>
@@ -46,18 +32,11 @@ const Header = ({ user }) => (
 	<Navbar bg='primary' variant='dark' expand='md'>
 		<Navbar.Brand>
             <Link to='/' style={linkStyle}>
-                react-auth-template
+                Allison Quador
             </Link>
         </Navbar.Brand>
 		<Navbar.Toggle aria-controls='basic-navbar-nav' />
 		<Navbar.Collapse id='basic-navbar-nav'>
-			<Nav className='ml-auto'>
-				{user && (
-					<span className='navbar-text mr-2'>Welcome, {user.email}</span>
-				)}
-				{alwaysOptions}
-				{user ? authenticatedOptions : unauthenticatedOptions}
-			</Nav>
 		</Navbar.Collapse>
 	</Navbar>
 )
